5729af1da26850895e68d7a8d1b27a9c221b5023
farrelzzzzzzz <farrelbalapradhana18@gmail.com>
2025-12-22 23:25:29 +0700

    update

---
+++ app/src/main/java/com/tugas_akhir/myapplication/Halaman1Activity.kt
@@ -0,0 +1,24 @@
+package com.tugas_akhir.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class Halaman1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Sesuai nama XML
+        setContentView(R.layout.halaman1_main)
+
+        val btnStart = findViewById<LinearLayout>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            // Pindah ke halaman berikutnya
+            val intent = Intent(this, Halaman2Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/tugas_akhir/myapplication/Halaman2Activity.java
@@ -0,0 +1,4 @@
+package com.tugas_akhir.myapplication;
+
+public class Halaman2Activity {
+}
